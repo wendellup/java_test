a92d6fd65ea96347aaa8713d22eb06d31b92ef0d
wendellup@163.com <wendellup@163.com>
2015-05-18 23:04:01 +0800

    Revert "commit"
    This reverts commit 6f7f0a43fc0ce8f97109e868016f6345d3cbeaa5.

--- OnlineJudge/src/com/leetcode/question28/Solution.java
+++
@@ -1,8 +0,0 @@
-package com.leetcode.question28;
-
-public class Solution {
-    public int strStr(String haystack, String needle) {
-    	return haystack.indexOf(needle);
-//        return -1;
-    }
-}
