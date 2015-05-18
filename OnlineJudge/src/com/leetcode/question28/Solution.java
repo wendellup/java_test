6f7f0a43fc0ce8f97109e868016f6345d3cbeaa5
wendellup@163.com <wendellup@163.com>
2015-05-18 23:03:55 +0800

    commit

---
+++ OnlineJudge/src/com/leetcode/question28/Solution.java
@@ -0,0 +1,8 @@
+package com.leetcode.question28;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+    	return haystack.indexOf(needle);
+//        return -1;
+    }
+}
