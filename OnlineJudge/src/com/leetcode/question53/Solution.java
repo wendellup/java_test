3b22ea9737a930225d69e9bf8239cebce23fa58a
wendellup@163.com <wendellup@163.com>
2015-08-26 07:19:45 +0800

    leetcode53

---
+++ OnlineJudge/src/com/leetcode/question53/Solution.java
@@ -0,0 +1,17 @@
+package com.leetcode.question53;
+
+public class Solution {
+	public int maxSubArray(int[] A) {
+        int[] sum = new int[A.length];
+        
+        int max = A[0];
+        sum[0] = A[0];
+ 
+        for (int i = 1; i < A.length; i++) {
+            sum[i] = Math.max(A[i], sum[i - 1] + A[i]);
+            max = Math.max(max, sum[i]);
+        }
+ 
+        return max;
+    }
+}
