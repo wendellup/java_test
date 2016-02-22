10e3b807ac21f40aeb749ecaf8cb0e2604070e0b
wendellup <wendellup@163.com>
2016-02-22 17:55:35 +0800

    dp练习:leetcode91
    dp练习:leetcode91

--- test.egame/src/test/client/file/FileTest.java
+++ test.egame/src/test/client/file/FileTest.java
@@ -19,8 +19,9 @@ public class FileTest {
 //		System.out.println(Utils.getFileId("47d5d513h221e8bd", 0));
 //		http://cdn.play.cn/f/pkg/gm/000/002/770/84bea3d0h2a4772a/5008336.apk
 		System.out.println(Utils.getFileId("84bea3d0h2a4772a", 0));
-//		System.out.println(Utils.getFileId("http://cdn.play.cn/f/pkg/gm/000/002/558/36c8d67bh2709e28/2017_RM_2.5.3.2_Channel_2017.apk", 0));
-//		System.out.println(Utils.getFileId("http://cdn.play.cn/f/pkg/gm/006399c276h1b8eeab/_RM_2.5.3.2_Channel_2017.apk", 0));
+		System.out.println(Utils.getFileId("http://cdn.play.cn/f/pkg/gm/000/002/558/36c8d67bh2709e28/2017_RM_2.5.3.2_Channel_2017.apk", 0));
+		System.out.println(Utils.getFileId("http://cdn.play.cn/f/pkg/gm/006399c276h1b8eeab/_RM_2.5.3.2_Channel_2017.apk", 0));
+		System.out.println(Utils.getFileId("/opt/data/cdn/efs/pkg/gm/000/002/558/006399c276h1b8eeab.apk", 0));
 		
 	}
 	
